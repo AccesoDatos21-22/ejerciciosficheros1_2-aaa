492f8236eb450ab185e1c55b59a01d38e2173d55
AlbertoJimenez2dam <ie2smr2@gmail.com>
2021-09-27 03:12:34 +0200

    testing push

--- src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
+++ src/org/madrid/ad/ut01/ficheros/FicherosTexto.java
@@ -2,64 +2,116 @@ package org.madrid.ad.ut01.ficheros;
 
 import org.madrid.ad.ut01.ficheros.interfaces.InterfazFicherosTexto;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 /**
- * 
- * @author Escribe_aqui_tu_nombre
- * @date 
- * @version 
+ * @author Alberto Jiménez
+ * @date 2021-9-27
+ * @version v1
  * @license GPLv3
- *
  */
 public class FicherosTexto implements InterfazFicherosTexto{
-
+	/**
+	 * Ejercicio 2
+	 * @param rutaFichero
+	 */
 	@Override
 	public void leer(String rutaFichero) {
-		// TODO Auto-generated method stub testo
-		
+		try (BufferedReader reader = new BufferedReader(new FileReader(rutaFichero))) {
+			while (true) {
+				int charInt = reader.read();
+				
+				if (charInt == -1) {
+					break;
+				}
+				
+				System.out.print((char)charInt);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichero " + rutaFichero + " no encontrado");
+		} catch (IOException e) {
+			System.out.println("El fichero " + rutaFichero + " no se pudo leer");
+		}
 	}
-
+	
+	/**
+	 * Ejercicio 4
+	 * @param rutaFichero
+	 * @return
+	 */
 	@Override
 	public int contarCaracteres(String rutaFichero) {
-		// TODO Auto-generated method stub
-		return 0;
+		int count = 0;
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(rutaFichero))) {
+			while (true) {
+				int charInt = reader.read();
+				
+				if (charInt == -1) {
+					break;
+				}
+				
+				if ((char)charInt == ' ') {
+					continue;
+				}
+				
+				count++;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichero " + rutaFichero + " no encontrado");
+		} catch (IOException e) {
+			System.out.println("El fichero " + rutaFichero + " no se pudo leer");
+		}
+		
+		return count;
 	}
-
+	
 	@Override
 	public int contarLineas(String rutaFichero) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
+	/**
+	 * Ejercicio 6
+	 * @param rutaFichero
+	 * @return
+	 */
 	@Override
 	public int contarPalabras(String rutaFichero) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
 	@Override
 	public int palabrasPentavocalica(String rutaFichero) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
+	/**
+	 * Ejercicio 8
+	 * @param rutaFichero
+	 * @return
+	 */
 	@Override
 	public int palabraMasLarga(String rutaFichero) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
 	@Override
 	public int frecuenciaVocales(String rutaFichero) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
+	/**
+	 * Ejercicio 10
+	 * @param rutaFichero
+	 * @return
+	 */
 	@Override
 	public int frecuenciaLetras(String rutaFichero) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
-
-
 }
--- src/org/madrid/ad/ut01/ficheros/Main.java
+++ src/org/madrid/ad/ut01/ficheros/Main.java
@@ -1,13 +1,20 @@
 package org.madrid.ad.ut01.ficheros;
 
-
-
 public class Main {
+	public static final String QUIJOTE = "bin/org/madrid/ad/ut01/ficheros/assets/el_quijote.txt";
 	
 	public static void main(String[] args) {
+		System.out.println("Directorio actual: " + System.getProperty("user.dir"));
 		
+		FicherosTexto ficherosTexto = new FicherosTexto();
 		
-
+		// Ejercicio 2
+		ficherosTexto.leer(QUIJOTE);
+		
+		// Padding
+		System.out.println("\n-----\n");
+		
+		// Ejercicio 4
+		System.out.println("El Quijote contiene " + ficherosTexto.contarCaracteres(QUIJOTE) + " caracteres");
 	}
-
 }
